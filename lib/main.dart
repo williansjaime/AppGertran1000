0056d3d8a00c48395788659236ae13ccfbd77eb7
hmneto91 <116448072+hmneto91@users.noreply.github.com>
2023-08-17 14:23:10 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,27 @@
-// @dart=2.9
-import 'dart:io';
+// import 'dart:io';
 import 'package:flutter/material.dart';
 import '../pages/login.page.dart';
 void main() async {  
-  HttpOverrides.global = new MyHttpOverrides();
+  // HttpOverrides.global = new MyHttpOverrides();
   runApp(AppGertran());
 }
 
 
-class MyHttpOverrides extends HttpOverrides{
-  @override
-  HttpClient createHttpClient(SecurityContext   context)
-  {
-    String ipexterno = 'gertranapi.hopto.org';
-    String ipinterno =  '192.168.34.7';
-    int port = 5000;
-    return super.createHttpClient(context)
-      ..badCertificateCallback = ((X509Certificate cert, String host, port) 
-      {        
-        final isValidHost = [Platform.isLinux? ipinterno: ipexterno,].contains(host); 
-        return isValidHost;
-      });
-  }
-}
+// class MyHttpOverrides extends HttpOverrides{
+//   @override
+//   HttpClient createHttpClient(SecurityContext   context)
+//   {
+//     String ipexterno = 'gertranapi.hopto.org';
+//     String ipinterno =  '192.168.34.7';
+//     int port = 5000;
+//     return super.createHttpClient(context)
+//       ..badCertificateCallback = ((X509Certificate cert, String host, port) 
+//       {        
+//         final isValidHost = [Platform.isLinux? ipinterno: ipexterno,].contains(host); 
+//         return isValidHost;
+//       });
+//   }
+// }
 
 class AppGertran extends StatelessWidget {
   @override
